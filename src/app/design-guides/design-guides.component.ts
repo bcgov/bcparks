7eedf92c0bd1d4fe1a4c1abdedd6193a379537d9
Cameron <cameron@digitalspace.ca>
2022-01-26 01:32:31 +0000

    BRS-328 updates

--- src/app/design-guides/design-guides.component.ts
+++ src/app/design-guides/design-guides.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-design-guides',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignGuidesComponent implements OnInit {
 
+  public relativeUrl = '/assets/objects/BC_Parks_Brand_Guidelines_Jan7_2022.pdf';
+  public siteUrl = 'https://bcgov.github.io/bcparks';
+  public pdfUrl = '.' + this.relativeUrl;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (environment.production) {
+      this.pdfUrl = this.siteUrl + this.relativeUrl;
+    }
   }
 
+  goToLink(url: string) {
+    window.open(url, "_blank");
+  }
 }
